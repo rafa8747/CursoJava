fa8515a87287aa5f56cc87eb80b7f48821d26d33
Raphael Avelar <rafael-avelar@hotmail.com>
2018-07-30 19:07:39 -0300

    Subindo Ambiente

---
+++ src/com/abctreinamentos/ClienteDAO.java
@@ -0,0 +1,129 @@
+package com.abctreinamentos;
+// Generated 29/07/2018 16:21:43 by Hibernate Tools 4.3.5.Final
+
+import java.io.File;
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Cliente.
+ * @see com.abctreinamentos.Cliente
+ * @author Hibernate Tools
+ */
+public class ClienteDAO {
+
+	private static final Log log = LogFactory.getLog(ClienteDAO.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		SessionFactory sessionFactory = new Configuration().
+				configure(new File("src/META-INF/hibernate.cfg.xml"))
+				.buildSessionFactory();
+				return sessionFactory;
+	}
+
+	public void persist(Cliente transientInstance) {
+		log.debug("persisting Cliente instance");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			session.persist(transientInstance);
+			session.getTransaction().commit();
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Cliente instance) {
+		log.debug("attaching dirty Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Cliente instance) {
+		log.debug("attaching clean Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Cliente persistentInstance) {
+		log.debug("deleting Cliente instance");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			session.delete(persistentInstance);
+			session.getTransaction().commit();
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public void merge(Cliente detachedInstance) {
+		log.debug("merging Cliente instance");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			session.persist(detachedInstance);
+			session.getTransaction().commit();
+			log.debug("merge successful");
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Cliente find(long cpf) {
+		log.debug("getting Cliente instance");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			session.beginTransaction();
+			Cliente instance = (Cliente) sessionFactory.getCurrentSession().get("com.abctreinamento.Cliente", cpf);
+			session.getTransaction().commit();		
+			
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {	
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Cliente instance) {
+		log.debug("finding Cliente instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("com.abctreinamentos.Cliente")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ src/com/abctreinamentos/CursoDAO.java
@@ -0,0 +1,116 @@
+package com.abctreinamentos;
+// Generated 29/07/2018 16:21:43 by Hibernate Tools 4.3.5.Final
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Curso.
+ * @see com.abctreinamentos.Curso
+ * @author Hibernate Tools
+ */
+public class CursoDAO {
+
+	private static final Log log = LogFactory.getLog(CursoDAO.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Curso transientInstance) {
+		log.debug("persisting Curso instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Curso instance) {
+		log.debug("attaching dirty Curso instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Curso instance) {
+		log.debug("attaching clean Curso instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Curso persistentInstance) {
+		log.debug("deleting Curso instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Curso merge(Curso detachedInstance) {
+		log.debug("merging Curso instance");
+		try {
+			Curso result = (Curso) sessionFactory.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Curso findById(java.math.BigDecimal id) {
+		log.debug("getting Curso instance with id: " + id);
+		try {
+			Curso instance = (Curso) sessionFactory.getCurrentSession().get("com.abctreinamentos.Curso", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Curso instance) {
+		log.debug("finding Curso instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("com.abctreinamentos.Curso")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
